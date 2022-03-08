e4bf91d3715c0e1f430eb3c30aa26ba0777ce111
Thomas Faulhaber <thomasihs365@gmail.com>
2022-03-07 19:15:49 -0800

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to babylonian
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/babylonian/core_test.clj
@@ -0,0 +1,7 @@
+(ns babylonian.core-test
+  (:require [clojure.test :refer :all]
+            [babylonian.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
