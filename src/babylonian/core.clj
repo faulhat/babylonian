13fce9b71a096366b5be85a0e1cff78e06c3743b
Thomas <thomasihs365@gmail.com>
2022-03-23 18:35:53 +0000

    Made it so that the program finds the root to a given number of decimal places, rather than doing a fixed number of iterations.

--- src/babylonian/core.clj
+++ src/babylonian/core.clj
@@ -5,19 +5,26 @@
 (defn next-guess [x guess]
   (/ (+ guess (/ x guess)) 2.0))
 
-;; Recursive function to do a given number of iterations.
-(defn iter-down [x guess upper i]
-  (let [new-guess (next-guess x guess)]
-    (println (str "Iteration " i ":"))
-    (println (str "(" guess " + " x "/" guess ") / 2 = " new-guess "\n"))
-    (if (< i upper)
-      (iter-down x new-guess upper (+ i 1)))))
+;; Multiply by 10^n and truncate to int
+(defn to-places [x n]
+  (int (* x (Math/pow 10 n))))
 
-;; Wrapper function to make n guesses starting with an initial random guess.
-(defn n-guesses [x n]
+;; Recursive function to find square root to a given number of decimal places
+(defn iter-down [x guess places i]
+  ;; Where i is the number of iterations so far
+  (let [new-guess (next-guess x guess)]
+    (println (str "Iteration " (+ i 1) ":"))
+    (println (str "(" guess " + " x "/" guess ") / 2 = " new-guess "\n"))
+    (let [truncated (to-places new-guess places)]
+      (if (not= (to-places guess places) truncated)
+        (iter-down x new-guess places (+ i 1))
+        (println (str "Final guess: " (/ truncated (Math/pow 10.0 places))))))))
+
+;; Wrapper function to make a guess to n places starting with an initial random guess.
+(defn get-guess [x n]
   (let [init-guess (* (rand) x)]
     (println (str "Initial guess: " init-guess "\n"))
-    (iter-down x init-guess n 1)))
+    (iter-down x init-guess n 0)))
 
 (defn -main [& args]
   ;; Ensure we have the right number of command line args
@@ -28,18 +35,18 @@
           (println "Error! Not enough command line arguments.")
           (println "Error! Too many command line arguments."))
         (println "\nUsage:")
-        (println "  sqrt [radicand] [n_iterations]")
-        (println "\nIf no value is provided for n_iterations, 10 iterations will be run by default.")
+        (println "  sqrt [radicand] [n_places]")
+        (println "\nIf no value is provided for n_places, the root will be found to 4 places by default.")
         (System/exit 1))
 
       ;; Now, number of args can only be 1 or 2
       (try
         (let [x (Double/parseDouble (first args))]
           (if (= n-args 1)
-            (n-guesses x 10) ;; Do ten iterations by default.
+            (get-guess x 4) ;; Four places by default
             (try ;; Number of args can only be 2
               (let [n (Integer/parseInt (second args))]
-                (n-guesses x n))
+                (get-guess x n))
               (catch NumberFormatException _
                 (println "Error! n value is not a valid integer.")
                 (println (str "Got: " (second args)))
